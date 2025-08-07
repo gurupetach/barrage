6ebc7ed9fc426e24fbc3ee1010203b9b252283fc
Peter Achieng <peterachieng01@gmail.com>
2025-08-07 14:28:31 +0300

    init

---
+++ barrage/lib/barrage.ex
@@ -0,0 +1,18 @@
+defmodule Barrage do
+  @moduledoc """
+  Documentation for `Barrage`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Barrage.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ barrage/test/barrage_test.exs
@@ -0,0 +1,8 @@
+defmodule BarrageTest do
+  use ExUnit.Case
+  doctest Barrage
+
+  test "greets the world" do
+    assert Barrage.hello() == :world
+  end
+end
